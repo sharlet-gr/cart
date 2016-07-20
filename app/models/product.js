0e093a6f3234a541a1f8f00f5fdc9c7c93d7345c
sharlet-gr <swilson@grepruby.com>
2016-07-20 14:51:12 +0530

    Changed directory structure

---
+++ app/models/product.js
@@ -0,0 +1,26 @@
+var mongoose=require('mongoose');
+var productSchema = new mongoose.Schema({
+	name:{
+		type: String,
+		required: true, 
+		unique: true
+	},
+	quantity:{
+		type: Number, 
+		required:true, 
+		min:0
+	},
+	price:{
+		type: Number, 
+		required:true, 
+		min:0
+	},
+	detail: String,
+	images: [String],
+	categories: [{
+		type: mongoose.Schema.Types.ObjectId,ref:'Category'
+	}]
+});
+
+mongoose.model('Product', productSchema);
+
